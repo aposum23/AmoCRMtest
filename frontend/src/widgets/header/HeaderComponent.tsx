461c65d5d7899263efc7a5eacea3155fa033f9ee
aposum23 <misha.duhno@mail.ru>
2023-11-28 21:43:27 +0300

    Test-01. Верстка хедера

--- frontend/src/widgets/header/HeaderComponent.tsx
+++ frontend/src/widgets/header/HeaderComponent.tsx
@@ -1,6 +1,10 @@
 import {HeaderButtonsStuct} from "./struct";
 import {ButtonType} from "./types";
 import RedirectButton from "../../shared/buttons/RedirectButton.tsx";
+import Welbex from '@/app/images/welbex.svg';
+import Telegram from '@/app/images/telegram.svg';
+import Viber from '@/app/images/viber.svg';
+import Whatsapp from '@/app/images/whatsapp.svg';
 
 import '@/app/styles/header/header.scss';
 
@@ -8,12 +12,20 @@ const HeaderComponent = () => {
 
     return (
         <div className='wrapper'>
+            <div className="header__logo">
+                <img src={Welbex} alt="Welbex logo"/>
+                <p>крупный интегратор CRM в России и ещё 8 странах</p>
+            </div>
             {
                 HeaderButtonsStuct.map((button: ButtonType) => {
                     return (<RedirectButton label={button.label} url={button.url} key={button.url}/>)
                 })
             }
+            <div className="spacer"></div>
             <p>+7 555 555-55-55</p>
+            <img src={Telegram} alt="Telegram" />
+            <img src={Viber} alt="Viber" />
+            <img src={Whatsapp} alt="Whatsapp" />
         </div>
     )
 }
